85939d5f8965f191feec0db0e436357e07c19030
LuisPozo-06 <luispg132@gmail.com>
2025-03-14 22:46:05 -0500

    Primer commit

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class frmPasajeros
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvPasajeros = new System.Windows.Forms.DataGridView();
+            this.bD_LATAMDataSet = new WindowsFormsApp8.BD_LATAMDataSet();
+            this.bDLATAMDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPasajeros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bD_LATAMDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDLATAMDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPasajeros
+            // 
+            this.dgvPasajeros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPasajeros.Location = new System.Drawing.Point(61, 48);
+            this.dgvPasajeros.Name = "dgvPasajeros";
+            this.dgvPasajeros.Size = new System.Drawing.Size(603, 200);
+            this.dgvPasajeros.TabIndex = 0;
+            this.dgvPasajeros.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPasajeros_CellContentClick);
+            // 
+            // bD_LATAMDataSet
+            // 
+            this.bD_LATAMDataSet.DataSetName = "BD_LATAMDataSet";
+            this.bD_LATAMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bDLATAMDataSetBindingSource
+            // 
+            this.bDLATAMDataSetBindingSource.DataSource = this.bD_LATAMDataSet;
+            this.bDLATAMDataSetBindingSource.Position = 0;
+            // 
+            // frmPasajeros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 311);
+            this.Controls.Add(this.dgvPasajeros);
+            this.Name = "frmPasajeros";
+            this.Text = "Pasajeros";
+            this.Load += new System.EventHandler(this.frmPasajeros_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPasajeros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bD_LATAMDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bDLATAMDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPasajeros;
+        private System.Windows.Forms.BindingSource bDLATAMDataSetBindingSource;
+        private BD_LATAMDataSet bD_LATAMDataSet;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class frmPasajeros : Form
+    {
+        public frmPasajeros()
+        {
+            InitializeComponent();
+        }
+
+
+        public void EliminarPasajero(Pasajero pPasajero)
+        {
+            try
+            {
+                using (BD_LATAMEntities bd = new BD_LATAMEntities())
+                {
+                    var pasajeros = from p in bd.Pasajero
+                                    where p.Id == pPasajero.Id
+                                    select p;
+
+                    foreach (Pasajero pas in pasajeros)
+                    {
+                        bd.Pasajero.Remove(pas);
+                    }
+
+                    bd.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void ActualizarPasajero(Pasajero pPasajero)
+        {
+            try
+            {
+                using (BD_LATAMEntities bd = new BD_LATAMEntities())
+                {
+                    var pasajeros = from p in bd.Pasajero
+                                    where p.Id == pPasajero.Id
+                                    select p;
+
+                    foreach (Pasajero pas in pasajeros)
+                    {
+                        pas.Nombres = pPasajero.Nombres;
+                        pas.ApellidoPaterno = pPasajero.ApellidoPaterno;
+                        pas.ApellidoMaterno = pPasajero.ApellidoMaterno;
+                        pas.Dni = pPasajero.Dni;
+                        pas.Activo = pPasajero.Activo;
+                    }
+
+                    bd.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        private void frmPasajeros_Load(object sender, EventArgs e)
+        {
+            Pasajero pasajero = new Pasajero();
+            pasajero.Id = 5;
+            //pasajero.Nombres = "Martha";
+            //pasajero.ApellidoPaterno = "Zegarra";
+            //pasajero.ApellidoMaterno = "Terrones";
+            //pasajero.Dni = "987654321";
+            //pasajero.Activo = false;
+
+            //InsertarPasajero(pasajero);
+            //ActualizarPasajero(pasajero);
+            EliminarPasajero(pasajero);
+            dgvPasajeros.DataSource = ObtenerPasajeros();
+
+
+
+        }
+
+        public void InsertarPasajero(Pasajero pPasajero)
+        {
+            try
+            {
+                using (BD_LATAMEntities bd = new BD_LATAMEntities())
+                {
+                    bd.Pasajero.Add(pPasajero);
+                    bd.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public List<PasajeroDto> ObtenerPasajeros()
+        {
+            try
+            {
+                using (BD_LATAMEntities bd = new BD_LATAMEntities())
+                {
+                    var consulta = (from n in bd.Pasajero
+                                    select new PasajeroDto
+                                    {
+                                        Id = n.Id,
+                                        Nombres = n.Nombres,
+                                        ApellidoPaterno = n.ApellidoPaterno,
+                                        ApellidoMaterno = n.ApellidoMaterno,
+                                        Dni = n.Dni,
+                                        Activo = n.Activo
+
+                                    }).ToList();
+                    return consulta;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void dgvPasajeros_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
