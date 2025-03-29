0a892728c568ba3c1b0b6f5e13fe612bddb69403
LuisPozo-06 <luispg132@gmail.com>
2025-03-28 20:25:25 -0500

    CRUD

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -35,17 +35,36 @@
             this.btnBuscar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidoMaterno = new System.Windows.Forms.TextBox();
+            this.txtApellidoPaterno = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.chkActivo = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPasajeros)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bD_LATAMDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bDLATAMDataSetBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvPasajeros
             // 
             this.dgvPasajeros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPasajeros.Location = new System.Drawing.Point(42, 64);
+            this.dgvPasajeros.Location = new System.Drawing.Point(7, 27);
             this.dgvPasajeros.Name = "dgvPasajeros";
-            this.dgvPasajeros.Size = new System.Drawing.Size(603, 200);
+            this.dgvPasajeros.Size = new System.Drawing.Size(552, 222);
             this.dgvPasajeros.TabIndex = 0;
             this.dgvPasajeros.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPasajeros_CellContentClick);
             // 
@@ -61,7 +80,7 @@
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(247, 23);
+            this.btnBuscar.Location = new System.Drawing.Point(231, 360);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 23);
             this.btnBuscar.TabIndex = 1;
@@ -72,7 +91,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 28);
+            this.label1.Location = new System.Drawing.Point(23, 365);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 13);
             this.label1.TabIndex = 2;
@@ -80,26 +99,192 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(92, 25);
+            this.txtNombre.Location = new System.Drawing.Point(76, 362);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(131, 20);
             this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombres:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Apellidos Maternos:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Apellidos Paternos:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Activo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "DNI:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(111, 36);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(47, 20);
+            this.txtId.TabIndex = 10;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(111, 69);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(133, 20);
+            this.txtNombres.TabIndex = 11;
+            // 
+            // txtApellidoMaterno
+            // 
+            this.txtApellidoMaterno.Location = new System.Drawing.Point(111, 135);
+            this.txtApellidoMaterno.Name = "txtApellidoMaterno";
+            this.txtApellidoMaterno.Size = new System.Drawing.Size(133, 20);
+            this.txtApellidoMaterno.TabIndex = 13;
+            // 
+            // txtApellidoPaterno
+            // 
+            this.txtApellidoPaterno.Location = new System.Drawing.Point(111, 102);
+            this.txtApellidoPaterno.Name = "txtApellidoPaterno";
+            this.txtApellidoPaterno.Size = new System.Drawing.Size(133, 20);
+            this.txtApellidoPaterno.TabIndex = 12;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(111, 168);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(133, 20);
+            this.txtDni.TabIndex = 14;
+            // 
+            // chkActivo
+            // 
+            this.chkActivo.AutoSize = true;
+            this.chkActivo.Location = new System.Drawing.Point(111, 204);
+            this.chkActivo.Name = "chkActivo";
+            this.chkActivo.Size = new System.Drawing.Size(15, 14);
+            this.chkActivo.TabIndex = 15;
+            this.chkActivo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNuevo);
+            this.groupBox1.Controls.Add(this.dgvPasajeros);
+            this.groupBox1.Location = new System.Drawing.Point(5, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(570, 296);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pasajeros";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnGuardar);
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Controls.Add(this.chkActivo);
+            this.groupBox2.Controls.Add(this.txtDni);
+            this.groupBox2.Controls.Add(this.txtApellidoMaterno);
+            this.groupBox2.Controls.Add(this.txtApellidoPaterno);
+            this.groupBox2.Controls.Add(this.txtNombres);
+            this.groupBox2.Controls.Add(this.txtId);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(581, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 296);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(83, 267);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 16;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(183, 267);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 17;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(484, 267);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 18;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // frmPasajeros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 311);
+            this.ClientSize = new System.Drawing.Size(880, 394);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.dgvPasajeros);
             this.Name = "frmPasajeros";
-            this.Text = "Pasajeros";
+            this.Text = "Mantenimiento de Pasajeros";
             this.Load += new System.EventHandler(this.frmPasajeros_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPasajeros)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bD_LATAMDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bDLATAMDataSetBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,6 +298,23 @@
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidoMaterno;
+        private System.Windows.Forms.TextBox txtApellidoPaterno;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.CheckBox chkActivo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
 
--- WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/Form1.cs
@@ -70,6 +70,18 @@ namespace WindowsFormsApp8
             }
         }
 
+        private void LimpiarFormulario()
+        {
+            txtId.Text = "";
+            txtNombres.Text = "";
+            txtApellidoPaterno.Text = "";
+            txtApellidoMaterno.Text = "";
+            txtDni.Text = "";
+            chkActivo.Checked = false;
+
+            txtNombres.Focus();
+        }
+
 
         private void frmPasajeros_Load(object sender, EventArgs e)
         {
@@ -159,9 +171,40 @@ namespace WindowsFormsApp8
             }
         }
 
+        private bool FormularioValido()
+        {
+            bool valido = false;
+
+            string nombres = txtNombres.Text.Trim();
+            string apellidoPaterno = txtApellidoPaterno.Text.Trim();
+            string apellidoMaterno = txtApellidoMaterno.Text.Trim();
+            string dni = txtDni.Text.Trim();
+
+            if (nombres.Length > 0 && apellidoPaterno.Length > 0 && apellidoMaterno.Length > 0 && dni.Length > 0)
+                valido = true;
+            return valido;
+        }
+
+
+
+
 
         private void dgvPasajeros_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if (e.RowIndex > -1 && e.ColumnIndex > -1 && dgvPasajeros.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dgvPasajeros.CurrentRow.Selected = true;
+
+                txtId.Text = dgvPasajeros.Rows[e.RowIndex].Cells["Id"].FormattedValue.ToString();
+                txtNombres.Text = dgvPasajeros.Rows[e.RowIndex].Cells["Nombres"].FormattedValue.ToString();
+                txtApellidoPaterno.Text = dgvPasajeros.Rows[e.RowIndex].Cells["ApellidoPaterno"].FormattedValue.ToString();
+                txtApellidoMaterno.Text = dgvPasajeros.Rows[e.RowIndex].Cells["ApellidoMaterno"].FormattedValue.ToString();
+                txtDni.Text = dgvPasajeros.Rows[e.RowIndex].Cells["Dni"].FormattedValue.ToString();
+
+                chkActivo.Checked = false;
+                if (dgvPasajeros.Rows[e.RowIndex].Cells["Activo"].FormattedValue.ToString() == "True")
+                    chkActivo.Checked = true;
+            }
 
         }
 
@@ -172,6 +215,103 @@ namespace WindowsFormsApp8
 
             // Cargar la lista 
             dgvPasajeros.DataSource = pasajeros;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+        }
+        public PasajeroDto ObtenerUltimoPasajero()
+        {
+            try
+            {
+                using (BD_LATAMEntities bd = new BD_LATAMEntities())
+                {
+                    var consulta = (from n in bd.Pasajero
+                                    orderby n.Id descending
+                                    select new PasajeroDto
+                                    {
+                                        Id = n.Id,
+                                        Nombres = n.Nombres,
+                                        ApellidoPaterno = n.ApellidoPaterno,
+                                        ApellidoMaterno = n.ApellidoMaterno,
+                                        Dni = n.Dni,
+                                        Activo = n.Activo
+
+                                    }).ToList();
+
+
+                    PasajeroDto pasajeroDto = consulta.First();
+
+                    return pasajeroDto;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (FormularioValido())
+            {
+                Pasajero pasajero = new Pasajero();
+                pasajero.Id = 0;
+                pasajero.Nombres = txtNombres.Text.Trim();
+                pasajero.ApellidoPaterno = txtApellidoPaterno.Text.Trim();
+                pasajero.ApellidoMaterno = txtApellidoMaterno.Text.Trim();
+                pasajero.Dni = txtDni.Text.Trim();
+                pasajero.Activo = chkActivo.Checked;
+
+                //Modificar Pasajero
+                if (txtId.Text.Trim().Length > 0)
+                {
+                    pasajero.Id = Convert.ToInt32(txtId.Text);
+                    ActualizarPasajero(pasajero);
+                }
+                //Nuevo Pasajero
+                else
+                {
+                    InsertarPasajero(pasajero);
+                    txtId.Text = ObtenerUltimoPasajero().Id.ToString();
+                }
+
+                MessageBox.Show("Datos guardados satisfactoriamente", "Ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                dgvPasajeros.DataSource = ObtenerPasajeros();
+            }
+            else
+            {
+                MessageBox.Show("Todos los datos son obligatorios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNombres.Focus();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text.Trim().Length > 0)
+            {
+                DialogResult respuestaAdvertencia = DialogResult.OK;
+                respuestaAdvertencia = MessageBox.Show("¿Está se guro de eliminar el pasajero?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (respuestaAdvertencia == DialogResult.Yes)
+                {
+                    Pasajero pasajero = new Pasajero();
+                    pasajero.Id = Convert.ToInt32(txtId.Text);
+
+                    EliminarPasajero(pasajero);
+                    dgvPasajeros.DataSource = ObtenerPasajeros();
+
+                    LimpiarFormulario();
+                    MessageBox.Show("Registro eliminado satisfactoriamente", "Ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar el registro que desea eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
